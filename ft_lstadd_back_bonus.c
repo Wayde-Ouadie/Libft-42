572820711c6a14ac8e14576e70212cda728ac6e9
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2023-11-10 19:41:34 +0100

    Only ft_lstmap left to be done
    All mandatory part finished and 8/9 bonus part done.

---
+++ ft_lstadd_back_bonus.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oel-feng <oel-feng@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/10 18:57:22 by oel-feng          #+#    #+#             */
+/*   Updated: 2023/11/10 19:03:06 by oel-feng         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (!lst || !new)
+		return ;
+	if (*lst != NULL)
+		ft_lstlast(*lst)->next = new;
+	else
+		*lst = new;
+}
