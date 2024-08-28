c52ff838becc5b630b9720a463dd8880911115f9
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2024-08-28 07:33:07 +0100

    Shorter version of putnbr_fd

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -12,21 +12,31 @@
 
 #include "libft.h"
 
+// void	ft_putnbr_fd(int n, int fd)
+// {
+// 	long	num;
+
+// 	num = n;
+// 	if (num < 0)
+// 	{
+// 		write(fd, "-", 1);
+// 		num *= -1;
+// 	}
+// 	if (num > 9)
+// 	{
+// 		ft_putnbr_fd(num / 10, fd);
+// 		ft_putchar_fd((num % 10) + 48, fd);
+// 	}
+// 	else
+// 		ft_putchar_fd(num + 48, fd);
+// }
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	long	num;
+	long int	nb;
 
-	num = n;
-	if (num < 0)
-	{
-		write(fd, "-", 1);
-		num *= -1;
-	}
-	if (num > 9)
-	{
-		ft_putnbr_fd(num / 10, fd);
-		ft_putchar_fd((num % 10) + 48, fd);
-	}
-	else
-		ft_putchar_fd(num + 48, fd);
+	nb = n;
+	(nb < 0) && (write(fd, "-", 1), nb *= -1);
+	(nb > 9) && (ft_putnbr_fd(nb / 10, fd), 0);
+	write(fd, &"0123456789"[nb % 10], 1);
 }
