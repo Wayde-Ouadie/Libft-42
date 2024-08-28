70dbb9b08be81485ae6d072e0f9fa9adfcdd8376
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2024-08-28 07:35:23 +0100

    Update ft_putnbr_fd.c

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -33,9 +33,7 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	long int	nb;
-
-	nb = n;
+	long int nb = n;
 	(nb < 0) && (write(fd, "-", 1), nb *= -1);
 	(nb > 9) && (ft_putnbr_fd(nb / 10, fd), 0);
 	write(fd, &"0123456789"[nb % 10], 1);
